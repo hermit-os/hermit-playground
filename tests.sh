c2c7cee69e49b5aee429dcece2b26176125a2fbc
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2019-06-27 21:30:08 +0200

    install lld before building the kernel

--- tests.sh
+++ tests.sh
@@ -6,7 +6,7 @@ OS_VERSION=$2
 export DEBIAN_FRONTEND="noninteractive"
 
 apt-get -qq update || exit 1
-apt-get install -y --no-install-recommends binutils bsdmainutils ca-certificates cmake curl gcc git libc-dev make nasm qemu-system-x86 rpm || exit 1
+apt-get install -y --no-install-recommends binutils bsdmainutils ca-certificates cmake curl gcc git libc-dev make nasm qemu-system-x86 lld rpm || exit 1
 
 echo "deb [trusted=yes] http://dl.bintray.com/hermitcore/ubuntu bionic main" >> /etc/apt/sources.list
 apt-get -qq update || exit 1
